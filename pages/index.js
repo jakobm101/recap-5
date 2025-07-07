f54e8daa100cc1c26d8fd68392de5f3d40647999
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-07 10:15:29 +0200

    fetching works

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
+import useSWR from "swr";
+
+const URL = 'https://example-apis.vercel.app/api/art'
+const fetcher = (url) => fetch(url).then(response => response.json())
+
 export default function HomePage() {
+  const { data } = useSWR(URL,()=>fetcher(URL))
+  console.log('fetched', data[0].slug);
+  
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+
+      <h1>'ART' Gallery</h1>
     </div>
   );
 }
