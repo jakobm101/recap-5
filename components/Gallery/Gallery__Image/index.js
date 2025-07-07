57bd34017058576d7df79d6d63db5d10febd67b8
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-07 15:18:58 +0200

    favorite button added and inline styles

--- components/Gallery/Gallery__Image/index.js
+++ components/Gallery/Gallery__Image/index.js
@@ -4,10 +4,11 @@ import { useRouter } from "next/router";
 export default function Gallery__Image({ image }) {
   const router = useRouter()
   return (
-    <>
+    <div style={{border: '1px black solid', padding: '5px', margin: '5px'}}>
       <Image onClick={()=> router.push(`/art-pieces/${image.slug}`)} src={image.imageSource} alt="Art" width={100} height={100} />
       <p>{image.name}</p>
       <p>{image.artist}</p>
-    </>
+    <button>favorite</button>
+    </div>
   );
 }
