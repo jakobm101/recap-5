de2b5b03310d53d77c8f930f0773e6b9b3291df9
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-07 13:05:22 +0200

    prop drill loading state

--- pages/_app.js
+++ pages/_app.js
@@ -5,12 +5,17 @@ const URL = "https://example-apis.vercel.app/api/art";
 const fetcher = (url) => fetch(url).then((response) => response.json());
 
 export default function App({ Component, pageProps }) {
-  const { data } = useSWR(URL, () => fetcher(URL));
+  const { data, isLoading, error } = useSWR(URL, () => fetcher(URL));
 
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} data={data} />
+      <Component
+        {...pageProps}
+        data={data}
+        isLoading={isLoading}
+        error={error}
+      />
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import Gallery__Images__List from "@/components/Gallery/Gallery__Images__List";
+import Spotlight from "@/components/Gallery/Gallery__Spotlight";
 
-export default function HomePage({ data }) {
+export default function Homepage({ data }) {
+  const randomIndex = (array = []) => {
+    const length = array.length;
+    return Math.floor(Math.random() * length);
+  };
 
   return (
     <div>
-      <h1> ART Gallery</h1>
+      <h1> Spotlight </h1>
+      {data && data.length > 0 && (<Spotlight image={data[randomIndex(data)]} />)}
       <Gallery__Images__List data={data} />
     </div>
   );
