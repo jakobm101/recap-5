6ed8b1a66ef6e798178550d0bf6489ff065d981d
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-07 11:38:30 +0200

    issue one done

--- components/Gallery/Gallery__Image/index.js
+++ components/Gallery/Gallery__Image/index.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
 
 export default function Gallery__Image({ image }) {
-  console.log("gal image", image);
-
+    console.log(image);
+    
+    
   return (
     <>
       <Image src={image.imageSource} alt="Art" width={100} height={100} />
+      <p>{image.name}</p>
+      <p>{image.artist}</p>
     </>
   );
 }
--- components/Gallery/Gallery__Images__List/index.js
+++ components/Gallery/Gallery__Images__List/index.js
@@ -1,17 +1,13 @@
+import { uid } from "react-uid";
 import Gallery__Image from "../Gallery__Image";
 
 export default function Gallery__Images__List({ data }) {
-  data?.map((image) => {
-    // console.log("image", image);
-  });
-
   if (data) {
     return (
       <>
         <h2>List</h2>
-        <Gallery__Image image={data[0]} />
         {data.map((image) => (
-          <Gallery__Image image={image} key={image.id} />
+          <Gallery__Image image={image} key={uid(image)} />
         ))}
       </>
     );
