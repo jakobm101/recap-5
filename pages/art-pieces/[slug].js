6ea9afb57a1ae2497d876c37e89dded736fcf4d3
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-07 14:52:38 +0200

    displaying all art infos

--- pages/art-pieces/[slug].js
+++ pages/art-pieces/[slug].js
@@ -1,16 +1,29 @@
 import { useRouter } from "next/router";
 import Image from "next/image";
+import { uid } from "react-uid";
 
 export default function ArtPiece({ data, isLoading }) {
   const router = useRouter();
   if (data && data.length > 0) {
-    const image = data.find((img) => img.slug === router.query.slug);
+    const { name, imageSource, artist, year, genre, colors } = data.find(
+      (img) => img.slug === router.query.slug
+    );
 
     return (
       <>
-        <h1>{image.name}</h1>
-        <Image height={500} width={500} alt="Art" src={image.imageSource} />
-        <p></p>
+        <h1>{name}</h1>
+        <Image height={500} width={500} alt="Art" src={imageSource} />
+        <p>
+          By {artist}, {year}, {genre}
+        </p>
+        <h4>Colors</h4>
+        {colors.map((color) => {
+          return (
+            <div style={{ color: color, display: 'inline'}} key={uid(color)}>
+              {color}
+            </div>
+          );
+        })}
       </>
     );
   } else if (isLoading) {
