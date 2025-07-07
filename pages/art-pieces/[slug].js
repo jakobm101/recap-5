8f637dfc3c894930257378de95fcca5996051efb
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-07 14:39:34 +0200

    Loading state is being processed by [slug]

--- pages/art-pieces/[slug].js
+++ pages/art-pieces/[slug].js
@@ -1,12 +1,21 @@
 import { useRouter } from "next/router";
+import Image from "next/image";
 
-export default function ArtPiece({ data }) {
+export default function ArtPiece({ data, isLoading }) {
   const router = useRouter();
-  console.log("slug", router.query.slug);
+  if (data && data.length > 0) {
+    const image = data.find((img) => img.slug === router.query.slug);
 
-  return (
-    <>
-      <h1>Art Piece [Slug]</h1>
-    </>
-  );
+    return (
+      <>
+        <h1>{image.name}</h1>
+        <Image height={500} width={500} alt="Art" src={image.imageSource} />
+        <p></p>
+      </>
+    );
+  } else if (isLoading) {
+    return "Loading";
+  } else {
+    return "Nothing to see here";
+  }
 }
