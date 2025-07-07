a982142007c0b27d51aa669637f5260bb227bd70
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-07 18:08:13 +0200

    bookmarked UI with emoji

--- components/Gallery/Gallery__Button__Favorite/index.js
+++ components/Gallery/Gallery__Button__Favorite/index.js
@@ -6,7 +6,7 @@ export default function Gallery__Button__Favorite({
   return (
     <>
       {/* to do: PROP DRILL */}
-      <button onClick={() => toggleFavorite(id)}>Favorite </button>
+      <button onClick={() => toggleFavorite(id)}>Favorite {favorites.find(fav => fav.id === id)?.isFav ? '📗': '📓'}</button>
     </>
   );
 }
