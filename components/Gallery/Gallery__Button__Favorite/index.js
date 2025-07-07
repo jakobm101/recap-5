fcd2925460a041b0aa1d1d43f10b138c955041aa
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-07 18:09:11 +0200

    cleanup

--- components/Gallery/Gallery__Button__Favorite/index.js
+++ components/Gallery/Gallery__Button__Favorite/index.js
@@ -1,12 +1,13 @@
 export default function Gallery__Button__Favorite({
-  id = "2",
+  id,
   toggleFavorite,
   favorites,
 }) {
   return (
     <>
-      {/* to do: PROP DRILL */}
-      <button onClick={() => toggleFavorite(id)}>Favorite {favorites.find(fav => fav.id === id)?.isFav ? '📗': '📓'}</button>
+      <button onClick={() => toggleFavorite(id)}>
+        Favorite {favorites.find((fav) => fav.id === id)?.isFav ? "📗" : "📓"}
+      </button>
     </>
   );
 }
