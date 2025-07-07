3cbafb6393d684705d68ebf5f8bae6d7a0230515
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-07 12:45:43 +0200

    random function works ... adding 'return' helps

--- components/Gallery/Gallery__Spotlight/index.js
+++ components/Gallery/Gallery__Spotlight/index.js
@@ -1,9 +1,14 @@
-export default function Spotlight({image}) {
-    console.log('Spotlight', image);
-    
-  return (
-    <>
-      <h2>Spotlight Image</h2>
-    </>
-  );
+export default function Spotlight({ image }) {
+  image ?? console.log("No image yet");
+  !image || console.log("image arrived", image);
+
+  if (image) {
+    return (
+      <>
+        <h2>Spotlight Image</h2>
+      </>
+    );
+  } else {
+    return "Loading";
+  }
 }
--- pages/spotlight.js
+++ pages/spotlight.js
@@ -1,15 +1,20 @@
 import Gallery__Images__List from "@/components/Gallery/Gallery__Images__List";
 import Spotlight from "@/components/Gallery/Gallery__Spotlight";
+import { useEffect } from "react";
 
 export default function Spotlight__Page({ data }) {
-  if (data) {
-    const image = data[0];
-    return (
-      <div>
-        <h1> Spotlight </h1>
-        <Spotlight image={image} />
-        <Gallery__Images__List data={data} />
-      </div>
-    );
-  } else {return "Loading"}
+  const randomIndex = (array = []) => {
+    const length = array.length;
+    const randomIndex = Math.floor(Math.random() * length);
+    console.log({ randomIndex });
+    return randomIndex
+  };
+
+  return (
+    <div>
+      <h1> Spotlight </h1>
+      {data && data.length > 0 && (<Spotlight image={data[randomIndex(data)]} />)}
+      <Gallery__Images__List data={data} />
+    </div>
+  );
 }
