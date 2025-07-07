e1e3bbc55d59315b64e10fd84e1900f3d07a1e45
Zebraloew <20402329+jakobm101@users.noreply.github.com>
2025-07-07 10:20:08 +0200

    prop drilling

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import GlobalStyle from "../styles";
+import useSWR from "swr";
+
+const URL = "https://example-apis.vercel.app/api/art";
+const fetcher = (url) => fetch(url).then((response) => response.json());
 
 export default function App({ Component, pageProps }) {
+  const { data } = useSWR(URL, () => fetcher(URL));
+
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component {...pageProps} data={data} />
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,9 @@
-import useSWR from "swr";
-
-const URL = 'https://example-apis.vercel.app/api/art'
-const fetcher = (url) => fetch(url).then(response => response.json())
-
-export default function HomePage() {
-  const { data } = useSWR(URL,()=>fetcher(URL))
-  console.log('fetched', data[0].slug);
+export default function HomePage({ data }) {
+  console.log("fetched", data?.[0].slug);
   
   return (
     <div>
-
-      <h1>'ART' Gallery</h1>
+      <h1> ART Gallery</h1>
     </div>
   );
 }
